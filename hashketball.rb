62ce1dd091f3df0738992e197abcb029de398c3b
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 17:31:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,7 +195,7 @@ end
 def find_player_with_longest_shoe()
   largest_shoe_size = nil
   player_with_largest_shoe = nil
-  
+
   game_hash().each do |location, team_data|
     team_data[:players].each do |player_name, player_stats|
       if largest_shoe_size == nil || player_stats[:shoe] > largest_shoe_size
