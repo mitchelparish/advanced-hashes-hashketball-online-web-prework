9bc4daeae8f89ab2daea6b6440e49b9475130e28
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 17:19:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,6 +139,11 @@ def shoe_size(name)
 end
 
 def team_colors(team)
+  game_hash().each do |location, team_data|
+    if team == team_data[:team_name]
+      return team_data[:colors]
+    end
+  end
 end
 
 def team_names
